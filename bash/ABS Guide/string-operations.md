f73bc9e2400ec4e485ba881b04ec5880b4a89ba8
cidlik <zubastikiko@gmail.com>
2023-10-07 13:35:39 +0300

    bash: Add chapter 3
    This chapter describes passing arguments in shell scripts.

---
+++ bash/ABS Guide/string-operations.md
@@ -0,0 +1,11 @@
+#bash #string
+
+# Ресурсы
+
+1. ABS Guide: https://tldp.org/LDP/abs/html/refcards.html#AEN22828
+
+# Заметки
+
+## Операции со строками
+
+Файл: ``string-ops.sh``
--- bash/Chapter1/var.sh
+++ bash/Chapter1/var.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+../block.sh "Environment and user variables scope"
 export FOO=BAR
-echo "echo \$FOO" > tmp.sh
+echo "echo FOO=\$FOO" > tmp.sh
 bash tmp.sh
 
 XYZ=ABC
-echo "echo \$XYZ" > tmp.sh
+echo "echo FOO=\$XYZ" > tmp.sh
 bash tmp.sh
 
 rm -f tmp.sh
